669b8077f48baebb85aada16b19d99cdcc5b349d
jiangwk <jiangwk@haoocai.com>
2016-05-27 10:20:32 +0800

    add unit test

---
+++ jscheduler-core/src/test/java/com/haoocai/jscheduler/core/AbstractBaseTest.java
@@ -0,0 +1,14 @@
+package com.haoocai.jscheduler.core;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Michael Jiang on 16/5/26.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(locations = {"classpath:application-test.xml"})
+public class AbstractBaseTest {
+
+}
