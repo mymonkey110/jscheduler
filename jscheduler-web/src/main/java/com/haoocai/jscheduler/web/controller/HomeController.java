db1cc7b6572bb33cb008ff8196b0c9b2b24d925e
jiangwk <jiangwk@haoocai.com>
2016-05-30 17:17:15 +0800

    fix home controller

--- jscheduler-web/src/main/java/com/haoocai/jscheduler/web/controller/HomeController.java
+++ jscheduler-web/src/main/java/com/haoocai/jscheduler/web/controller/HomeController.java
@@ -1,32 +1,21 @@
 package com.haoocai.jscheduler.web.controller;
 
-import com.haoocai.jscheduler.core.ErrorCode;
-import com.haoocai.jscheduler.core.app.AppService;
-import com.haoocai.jscheduler.web.CommonResult;
+import com.haoocai.jscheduler.core.JschedulerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
-import java.util.List;
 
 /**
  * @author Michael Jiang on 16/5/24.
  */
 @Controller
 public class HomeController {
-
-    private final AppService appService;
-
     @Autowired
-    public HomeController(AppService appService) {
-        this.appService = appService;
-    }
-
+    private JschedulerConfig jschedulerConfig;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String dashboard(Model model) {
@@ -34,10 +23,4 @@ public class HomeController {
         return "index";
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/getApp/{namespace}", method = RequestMethod.GET)
-    public CommonResult getNamespaceApps(@PathVariable String namespace) {
-        List<String> apps = appService.getNamespaceApps(namespace);
-        return new CommonResult<>(ErrorCode.SUCCESS, apps);
-    }
 }
