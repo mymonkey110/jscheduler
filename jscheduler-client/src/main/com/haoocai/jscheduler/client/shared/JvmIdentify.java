544d7a63bace748d6673f037e58807e6ea4f5fee
jiangwk <jiangwk@haoocai.com>
2016-06-30 16:06:08 +0800

    add apache license header 2.0

--- jscheduler-client/src/main/com/haoocai/jscheduler/client/shared/JvmIdentify.java
+++ jscheduler-client/src/main/com/haoocai/jscheduler/client/shared/JvmIdentify.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016  Michael Jiang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.haoocai.jscheduler.client.shared;
 
 import java.lang.management.ManagementFactory;
@@ -11,9 +27,9 @@ import java.util.Enumeration;
  * @author Michael Jiang on 6/30/16.
  */
 public class JvmIdentify {
-    public final static int pid;
-    public final static String ip;
-    public final static String hostname;
+    private final static int pid;
+    private final static String ip;
+    private final static String hostname;
 
     static {
         String processID = ManagementFactory.getRuntimeMXBean().getName();
--- jscheduler-client/src/main/com/haoocai/jscheduler/client/task/TaskRegister.java
+++ jscheduler-client/src/main/com/haoocai/jscheduler/client/task/TaskRegister.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016  Michael Jiang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.haoocai.jscheduler.client.task;
 
 import com.haoocai.jscheduler.client.shared.JvmIdentify;
@@ -17,16 +33,12 @@ import java.util.concurrent.ConcurrentHashMap;
 class TaskRegister {
     private final Map<String, TaskWatcher> taskWatcherMap = new ConcurrentHashMap<>();
 
-    private final String namespace;
-    private final String app;
     private final ZKClient zkClient;
     private final String pathPrefix;
 
     private static Logger LOG = LoggerFactory.getLogger(TaskRegister.class);
 
     TaskRegister(String namespace, String app, ZKClient zkClient) {
-        this.namespace = Validate.checkNotNull(namespace);
-        this.app = Validate.checkNotNull(app);
         this.zkClient = Validate.checkNotNull(zkClient);
         this.pathPrefix = "/" + namespace + "/" + app;
     }
